ef0c73796776d2288b5c8db92e2c7ae14f24982a
Gabriel Dahia <gdahia@users.noreply.github.com>
2026-06-28 17:06:06 -0300

    Uncomment Section7 import

--- ExponentialRamsey.lean
+++ ExponentialRamsey.lean
@@ -27,7 +27,7 @@ import ExponentialRamsey.Prereq.RamseySmall
 import ExponentialRamsey.Section4
 import ExponentialRamsey.Section5
 import ExponentialRamsey.Section6
--- import ExponentialRamsey.Section7
+import ExponentialRamsey.Section7
 -- import ExponentialRamsey.Section8
 -- import ExponentialRamsey.Section9
 -- import ExponentialRamsey.Section10
