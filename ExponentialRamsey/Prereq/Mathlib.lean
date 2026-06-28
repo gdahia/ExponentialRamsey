659863883dd6cefd4ba7e6e9e60ebb23b085fa1b
Gabriel Dahia <gdahia@users.noreply.github.com>
2026-06-27 23:17:22 -0300

    Delete ExponentialRamsey/Prereq/Mathlib/Algebra/Order/Monoid/Lemmas.lean

--- ExponentialRamsey/Prereq/Mathlib.lean
+++ ExponentialRamsey/Prereq/Mathlib.lean
@@ -1,5 +1,4 @@
 import ExponentialRamsey.Prereq.Mathlib.Algebra.BigOperators.Ring
-import ExponentialRamsey.Prereq.Mathlib.Algebra.Order.Monoid.Lemmas
 import ExponentialRamsey.Prereq.Mathlib.Analysis.Calculus.IteratedDeriv.Lemmas
 import ExponentialRamsey.Prereq.Mathlib.Analysis.Calculus.MeanValue
 import ExponentialRamsey.Prereq.Mathlib.Analysis.Calculus.Taylor
